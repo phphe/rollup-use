20467023c1de133c3e0cd3d5cfc1dbaeb663ecdb
phphe <phphe@outlook.com>
2021-08-14 13:46:34 +0800

    WIP: vue3-ts

--- packages/vue3-ts/__tests__/vue3-ts.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const vue3Ts = require('..');
-
-describe('@rollup-use/vue3-ts', () => {
-    it('needs tests');
-});
--- packages/vue3-ts/lib/vue3-ts.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = vue3Ts;
-
-function vue3Ts() {
-    // TODO
-}
